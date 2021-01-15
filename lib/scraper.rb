80c4b63ba6cc173471f58e396917e64688d019b5
Owen Cruse <ocruse7@gmail.com>
2021-01-15 22:53:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,31 +4,39 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    index_page = Nokogiri::HTML(open(index_url))
-    students = []
-    index_page.css("div.roster-cards-container").each do |card|
-      card.css(".card-text-container").each do |container|
-        student_name = container.css(".student-name").text
-        student_location = container.css(".student-location").text
-        students << {name: student_name, location: student_location}
-      end
-    end
-    students
+		index_page = Nokogiri::HTML(open(index_url))
+		students = []
+		index_page.css("div.roster-cards-container").each do |card|
+			card.css(".student-card a").each do |student|
+				student_profile_link = "./fixtures/student-site/#{student.attr('href')}"
+			  student_location = student.css('.student-location').text
+			  student_name = student.css('.student-name').text
+			  students << {name: student_name, location: student_location, profile_url: student_profile_link}
+			end
+		end
+		
+		students
   end
 
   def self.scrape_profile_page(profile_url)
-    profile_page = Nokogiri::HTML(open(profile_url))
-    student = {}
+		student_profile = Nokogiri::HTML(open(profile_url))
+		student = {}
+		links = student_profile.css(".social-icon-container").children.css("a").map { |object| object.attribute('href').value}
+		links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("github")
+        student[:github] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      else
+        student[:blog] = link
+      end
+    end
+		student[:profile_quote] = student_profile.css(".profile-quote").text if student_profile.css(".profile-quote")
+		student[:bio] = student_profile.css("div.bio-content.content-holder div.description-holder p").text if student_profile.css("div.bio-content.content-holder div.description-holder p")
 
-    student[:twitter] = profile_page.css(".social-icon-container").children.css("a")[0].attribute("href").value
-    student[:linkedin] = profile_page.css(".social-icon-container").children.css("a")[1].attribute("href").value
-    student[:github] = profile_page.css(".social-icon-container").children.css("a")[2].attribute("href").value
-    student[:blog] = profile_page.css(".social-icon-container").children.css("a")[3].attribute("href").value
-
-    student[:profile_quote] = profile_page.css(".profile-quote").text
-    student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text
-
-    student
+		student
   end
 
 end
